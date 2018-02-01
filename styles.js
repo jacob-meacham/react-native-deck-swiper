34b8299a37f6f3c6f9d3d36b2034481e7ffa3253
Jacob Meacham <jmeacham@classpass.com>
2018-01-31 22:52:55 -0500

    Use flex styles for the container.

--- styles.js
+++ styles.js
@@ -7,11 +7,8 @@ const styles = StyleSheet.create({
   },
   container: {
     alignItems: 'stretch',
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0
+    flex: 1,
+    alignSelf: 'stretch'
   },
   childrenViewStyle: {
     position: 'absolute',
